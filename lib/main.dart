9a31ced7586b0db5bb95c6c1cbe3ad42edb911fa
Sobhie <sobhie.saad1@gmail.com>
2022-03-09 21:36:27 +0300

    task model, provider and extensions with local storage

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
-import 'src/app.dart';
+import 'package:tasks_app/app/data/services/storage/services.dart';
+import 'package:tasks_app/app/modules/home/view.dart';
 import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
 
@@ -18,8 +20,21 @@ void main() async {
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
 
   await GetStorage.init();
+  await Get.putAsync(() => StorageService().init());
   // Run the app and pass in the SettingsController. The app listens to the
   // SettingsController for changes, then passes it further down to the
   // SettingsView.
-  runApp(MyApp(settingsController: settingsController));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      title: "Tasks app",
+      home: HomePage(),
+    );
+  }
 }
