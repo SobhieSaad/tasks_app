d9e7160231e24d01eb1f1222072acb17f3a82a90
Sobhie <sobhie.saad1@gmail.com>
2022-03-13 23:22:08 +0300

    Home screen view tasks list after add

--- lib/app/modules/home/view.dart
+++ lib/app/modules/home/view.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:tasks_app/app/core/values/colors.dart';
+import 'package:tasks_app/app/data/models/task.dart';
 import 'package:tasks_app/app/modules/home/controller.dart';
 import 'package:tasks_app/app/core/utils/extension.dart';
+import 'package:tasks_app/app/modules/home/widgets/add_card.dart';
+import 'package:tasks_app/app/modules/home/widgets/task_card.dart';
 
 class HomePage extends GetView<HomeController> {
   const HomePage({Key? key}) : super(key: key);
@@ -19,6 +23,19 @@ class HomePage extends GetView<HomeController> {
             style: TextStyle(fontSize: 24.0.sp, fontWeight: FontWeight.bold),
           ),
         ),
+        Obx(
+          () => GridView.count(
+            crossAxisCount: 2,
+            shrinkWrap: true,
+            physics: const ClampingScrollPhysics(),
+            children: [
+              ...controller.tasks
+                  .map((element) => TaskCard(task: element))
+                  .toList(),
+              AddCard(),
+            ],
+          ),
+        )
       ],
     )));
   }
