0d9503574129045e59fc1dc40dbce75b359a9866
Sobhie <sobhie.saad1@gmail.com>
2022-04-09 16:16:52 +0300

    Report page

--- lib/app/modules/home/controller.dart
+++ lib/app/modules/home/controller.dart
@@ -15,6 +15,7 @@ class HomeController extends GetxController {
   final task = Rx<Task?>(null);
   final doingTodos = <dynamic>[].obs;
   final doneTodos = <dynamic>[].obs;
+  final tabIndex = 0.obs;
 
   @override
   void onInit() {
@@ -138,6 +139,34 @@ class HomeController extends GetxController {
     for (int i = 0; i < task.todos!.length; i++) {
       if (task.todos![i]['done'] == true) {
         res = 1;
+      }
+    }
+    return res;
+  }
+
+  void changeTabIndex(int index) {
+    tabIndex.value = index;
+  }
+
+  int getTotalTasks() {
+    var res = 0;
+    for (int i = 0; i < tasks.length; i++) {
+      if (tasks[i].todos != null) {
+        res += tasks[i].todos!.length;
+      }
+    }
+    return res;
+  }
+
+  int getTotalDoneTasks() {
+    var res = 0;
+    for (int i = 0; i < tasks.length; i++) {
+      if (tasks[i].todos != null) {
+        for (int j = 0; j < tasks[i].todos!.length; j++) {
+          if (tasks[i].todos![j]['done'] == true) {
+            res++;
+          }
+        }
       }
     }
     return res;
