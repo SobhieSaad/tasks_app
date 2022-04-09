ad908072d9df6acd0e847b3dda4a4ed80abd3620
Sobhie <sobhie.saad1@gmail.com>
2022-04-09 16:17:11 +0300

    ignore back system button  for Android

--- lib/app/modules/home/widgets/add_dialog.dart
+++ lib/app/modules/home/widgets/add_dialog.dart
@@ -10,140 +10,144 @@ class AddDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Form(
-        key: homeController.formKey,
-        child: ListView(
-          children: [
-            Padding(
-              padding: EdgeInsets.all(3.0.wp),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  IconButton(
-                    onPressed: () {
-                      Get.back();
-                      homeController.editController.clear();
-                      homeController.changeTask(null);
-                    },
-                    icon: const Icon(Icons.close),
-                  ),
-                  TextButton(
-                    style: ButtonStyle(
-                      overlayColor:
-                          MaterialStateProperty.all(Colors.transparent),
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        body: Form(
+          key: homeController.formKey,
+          child: ListView(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(3.0.wp),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        Get.back();
+                        homeController.editController.clear();
+                        homeController.changeTask(null);
+                      },
+                      icon: const Icon(Icons.close),
                     ),
-                    onPressed: () {
-                      if (homeController.formKey.currentState!.validate()) {
-                        if (homeController.task.value == null) {
-                          EasyLoading.showError('Please select task type');
-                        } else {
-                          var success = homeController.updateTask(
-                            homeController.task.value!,
-                            homeController.editController.text,
-                          );
-                          if (success) {
-                            EasyLoading.showSuccess(
-                                'Todo item added successfully');
-                            Get.back();
-                            homeController.changeTask(null);
-                          } else {
-                            EasyLoading.showError('Todo item already exists');
-                          }
-                          homeController.editController.clear();
-                        }
-                      }
-                    },
-                    child: Text(
-                      'Done',
-                      style: TextStyle(fontSize: 14.0.sp),
-                    ),
-                  )
-                ],
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 5.0.wp),
-              child: Text(
-                'New Task',
-                style:
-                    TextStyle(fontSize: 20.0.sp, fontWeight: FontWeight.bold),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 5.0.wp),
-              child: TextFormField(
-                controller: homeController.editController,
-                decoration: InputDecoration(
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.grey[400]!),
-                  ),
-                ),
-                autofocus: true,
-                validator: (value) {
-                  if (value == null || value.trim().isEmpty) {
-                    return 'Please enter your todo item';
-                  }
-                  return null;
-                },
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(
-                top: 5.0.wp,
-                left: 5.0.wp,
-                right: 5.0.wp,
-                bottom: 2.0.wp,
-              ),
-              child: Text(
-                'Add to',
-                style: TextStyle(
-                  fontSize: 14.0.sp,
-                  color: Colors.grey,
-                ),
-              ),
-            ),
-            ...homeController.tasks
-                .map((element) => Obx(
-                      () => InkWell(
-                        onTap: () {
-                          homeController.changeTask(element);
-                        },
-                        child: Padding(
-                          padding: EdgeInsets.symmetric(
-                            vertical: 3.0.wp,
-                            horizontal: 5.0.wp,
-                          ),
-                          child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Row(
-                                  children: [
-                                    Icon(
-                                      IconData(
-                                        element.icon,
-                                        fontFamily: 'MaterialIcons',
-                                      ),
-                                      color: HexColor.fromHex(element.color),
-                                    ),
-                                    SizedBox(width: 3.0.wp),
-                                    Text(
-                                      element.title,
-                                      style: TextStyle(
-                                        fontSize: 12.0.sp,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                if (homeController.task.value == element)
-                                  const Icon(Icons.check, color: Colors.blue)
-                              ]),
-                        ),
+                    TextButton(
+                      style: ButtonStyle(
+                        overlayColor:
+                            MaterialStateProperty.all(Colors.transparent),
                       ),
-                    ))
-                .toList()
-          ],
+                      onPressed: () {
+                        if (homeController.formKey.currentState!.validate()) {
+                          if (homeController.task.value == null) {
+                            EasyLoading.showError('Please select task type');
+                          } else {
+                            var success = homeController.updateTask(
+                              homeController.task.value!,
+                              homeController.editController.text,
+                            );
+                            if (success) {
+                              EasyLoading.showSuccess(
+                                  'Todo item added successfully');
+                              Get.back();
+                              homeController.changeTask(null);
+                            } else {
+                              EasyLoading.showError('Todo item already exists');
+                            }
+                            homeController.editController.clear();
+                          }
+                        }
+                      },
+                      child: Text(
+                        'Done',
+                        style: TextStyle(fontSize: 14.0.sp),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 5.0.wp),
+                child: Text(
+                  'New Task',
+                  style:
+                      TextStyle(fontSize: 20.0.sp, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 5.0.wp),
+                child: TextFormField(
+                  controller: homeController.editController,
+                  decoration: InputDecoration(
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey[400]!),
+                    ),
+                  ),
+                  autofocus: true,
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return 'Please enter your todo item';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                  top: 5.0.wp,
+                  left: 5.0.wp,
+                  right: 5.0.wp,
+                  bottom: 2.0.wp,
+                ),
+                child: Text(
+                  'Add to',
+                  style: TextStyle(
+                    fontSize: 14.0.sp,
+                    color: Colors.grey,
+                  ),
+                ),
+              ),
+              ...homeController.tasks
+                  .map((element) => Obx(
+                        () => InkWell(
+                          onTap: () {
+                            homeController.changeTask(element);
+                          },
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(
+                              vertical: 3.0.wp,
+                              horizontal: 5.0.wp,
+                            ),
+                            child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Row(
+                                    children: [
+                                      Icon(
+                                        IconData(
+                                          element.icon,
+                                          fontFamily: 'MaterialIcons',
+                                        ),
+                                        color: HexColor.fromHex(element.color),
+                                      ),
+                                      SizedBox(width: 3.0.wp),
+                                      Text(
+                                        element.title,
+                                        style: TextStyle(
+                                          fontSize: 12.0.sp,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  if (homeController.task.value == element)
+                                    const Icon(Icons.check, color: Colors.blue)
+                                ]),
+                          ),
+                        ),
+                      ))
+                  .toList()
+            ],
+          ),
         ),
       ),
     );
